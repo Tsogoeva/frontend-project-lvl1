3c699afafd73992c725af961043338ff9b290b1c
Tsogoeva Tatiana <tsogoevats@gmail.com>
2021-12-17 20:46:44 +0300

    add function-meet and call

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+
+import { meet } from '../src/cli.js';
+
+meet();
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!');
+export const meet = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + name + '!');
+};
