a5f31578a78e5699ad6705138583b7a3bfb38bd1
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-03 08:24:58 +0300

    add logic to gcd-game.js

---
+++ src/games/gcd-game.js
@@ -0,0 +1,20 @@
+import getRandomNumber from '../random-number.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (randomNum1, randomNum2) => {
+  if (!randomNum2) {
+    return randomNum1;
+  }
+  return getGcd(randomNum2, randomNum1 % randomNum2);
+};
+
+const getQuestionAndAnswer = () => {
+  const randomNum1 = getRandomNumber();
+  const randomNum2 = getRandomNumber();
+  const question = `${randomNum1} ${randomNum2}`;
+  const correctAnswer = getGcd(randomNum1, randomNum2);
+  return [question, correctAnswer];
+};
+
+export { rules, getQuestionAndAnswer };
