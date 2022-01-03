9e1415a55f1ca05d168d89e2e23922d347216553
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-03 17:31:38 +0300

    fix progression-game

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -2,28 +2,23 @@ import getRandomNumber from '../random-number.js';
 
 const rules = 'What number is missing in the progression?';
 
-const firstRandomNum = getRandomNumber(1, 100);
-const randomNumOfSteps = getRandomNumber(5, 10);
-const randomProgression = getRandomNumber(1, 10);
-
 const getProgression = () => {
+  const firstRandomNum = getRandomNumber(1, 100);
+  const randomNumOfSteps = getRandomNumber(5, 10);
+  const randomProgression = getRandomNumber(1, 10);
   const progression = [];
   for (let i = 0; i <= randomNumOfSteps; i += 1) {
-    if (progression.length === randomNumOfSteps) {
-      return progression;
-    }
     progression.push(firstRandomNum + randomProgression * i);
   }
-  return progression;
+  const puzzledIndex = getRandomNumber(0, randomNumOfSteps - 1);
+  const puzzledNumber = progression[puzzledIndex];
+  progression[puzzledIndex] = '..';
+  return [progression, puzzledNumber];
 };
 
-const puzzledIndex = getRandomNumber(0, randomNumOfSteps - 1);
-const puzzledNumber = getProgression().splice(puzzledIndex, 1);
-const puzzledProgression = getProgression();
-puzzledProgression[puzzledIndex] = '..';
-
 const getQuestionAndAnswer = () => {
-  const question = puzzledProgression.join(', ');
+  const [progression, puzzledNumber] = getProgression();
+  const question = progression.join(', ');
   const correctAnswer = String(puzzledNumber);
   return [question, correctAnswer];
 };
