3147e632e4f89ae47d93b41c4e6ddfb4b6c800ec
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-04 13:43:27 +0300

    fix name number

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -4,9 +4,9 @@ const checkEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const number = getRandomNumber(1, 100);
-  const question = number;
-  const correctAnswer = checkEven(number);
+  const checkDigit = getRandomNumber(1, 100);
+  const question = checkDigit;
+  const correctAnswer = checkEven(checkDigit);
   return [question, correctAnswer];
 };
 
