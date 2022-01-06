e0bc6c72e9d3d99c561aa3f3120beca4f56e624d
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-06 11:02:26 +0300

    fix checkPrime, add getCorrectAnswer and fix getQuestionAndAnswer

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -5,16 +5,17 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const checkPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
+const getCorrectAnswer = (num) => (checkPrime(num) === false ? 'no' : 'yes');
+
 const getQuestionAndAnswer = () => {
-  const checkDigit = getRandomNumber(1, 100);
-  const question = checkDigit;
-  const correctAnswer = checkPrime(checkDigit);
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = getCorrectAnswer(question);
   return [question, correctAnswer];
 };
 
