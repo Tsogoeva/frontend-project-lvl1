91498813e953889fc89234004963da6a731db61f
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-06 11:03:45 +0300

    fix checkEven, add getCorrectAnswer and fix getQuestionAndAnswer

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,12 +1,14 @@
 import getRandomNumber from '../random-number.js';
 
-const checkEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const checkEven = (num) => (num % 2 === 0);
+
+const getCorrectAnswer = (num) => (checkEven(num) === true ? 'yes' : 'no');
+
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
-  const checkDigit = getRandomNumber(1, 100);
-  const question = checkDigit;
-  const correctAnswer = checkEven(checkDigit);
+  const question = getRandomNumber(1, 100);
+  const correctAnswer = getCorrectAnswer(question);
   return [question, correctAnswer];
 };
 
