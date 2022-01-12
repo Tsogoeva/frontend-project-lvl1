8dca26719246c295d54867e414a98cd0f14e23c6
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-12 17:10:23 +0300

    fix getQuestionAndAnswer: const operator

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -5,13 +5,6 @@ const rules = 'What is the result of the expression?';
 
 const operators = ['+', '-', '*'];
 
-const getRandomOperator = () => {
-  const minIndex = 0;
-  const maxIndex = operators.length - 1;
-  const randomIndex = getRandomNumber(minIndex, maxIndex);
-  return operators[randomIndex];
-};
-
 const calculation = (randomNum1, operator, randomNum2) => {
   switch (operator) {
     case '+':
@@ -28,7 +21,7 @@ const calculation = (randomNum1, operator, randomNum2) => {
 const getQuestionAndAnswer = () => {
   const randomNum1 = getRandomNumber(1, 10);
   const randomNum2 = getRandomNumber(1, 10);
-  const operator = getRandomOperator();
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
   const question = `${randomNum1} ${operator} ${randomNum2}`;
   const correctAnswer = String(calculation(randomNum1, operator, randomNum2));
   return [question, correctAnswer];
