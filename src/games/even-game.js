0facc203bb4fe175f6b07709d01006424742e4f7
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-01-13 19:49:36 +0300

    fix correctAnswer

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -3,13 +3,11 @@ import getRandomNumber from '../random-number.js';
 
 const isEven = (num) => (num % 2 === 0);
 
-const getCorrectAnswer = (num) => (isEven(num) === true ? 'yes' : 'no');
-
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = getCorrectAnswer(question);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -12,11 +12,9 @@ const isPrime = (num) => {
   return true;
 };
 
-const getCorrectAnswer = (num) => (isPrime(num) === false ? 'no' : 'yes');
-
 const getQuestionAndAnswer = () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = getCorrectAnswer(question);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
